836762725bf1e6228d1973fd64da9773a46e8289
Arthur Vardanyan <artie.vard@gmail.com>
2013-12-15 00:01:56 +0400

    rollback rebuild.sh because used from publish.sh

--- rebuild.sh
+++ rebuild.sh
@@ -1,8 +1,6 @@
 #!/usr/bin/env zsh
 
 print -- "Compiling and building"
-ghc --make site.hs
 cabal install && \
-./site  clean && \
-./site  build && \
-./site  watch
+./site clean && \
+./site build
